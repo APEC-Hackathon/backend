2049a7aa1a78c0d3710a9eb3fcb599b49dad2173
nvbinh15 <binh.pdc01@gmail.com>
2023-05-26 16:57:47 +0200

    Check collaboration source, add problem endpoints

--- app/api/api_v1/endpoints/collaboration.py
+++ app/api/api_v1/endpoints/collaboration.py
@@ -19,6 +19,11 @@ def create_collaboration(
     """
     Create new collaboration.
     """
+    if collaboration_in.source_id is not None:
+        print('Yes')
+        source_problem = crud.problem.get(db=db, id=collaboration_in.source_id)
+        if not source_problem:
+            raise HTTPException(status_code=404, detail='Source problem not found')
     collaboration = crud.collaboration.create_with_owner(
         db=db, obj_in=collaboration_in, owner_id=current_user.id
     )
---
+++ app/api/api_v1/endpoints/problem.py
@@ -0,0 +1,112 @@
+from typing import Any, List 
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app import crud, models, schemas
+from app.api import deps
+
+router = APIRouter()
+
+
+@router.post('/', response_model=schemas.Problem)
+def create_problem(
+    *,
+    db: Session = Depends(deps.get_db),
+    problem_in: schemas.ProblemCreate,
+    current_user: models.User = Depends(deps.get_current_user),
+) -> Any:
+    """
+    Create new Problem.
+    """
+    Problem = crud.problem.create_with_owner(
+        db=db, obj_in=problem_in, owner_id=current_user.id
+    )
+    return Problem
+
+
+@router.get('/', response_model=List[schemas.Problem])
+def read_my_problems(
+    db: Session = Depends(deps.get_db),
+    skip: int = 0,
+    limit: int = 100,
+    current_user: models.User = Depends(deps.get_current_user),
+) -> Any:
+    """
+    Retrieve my Problems.
+    """
+    Problems = crud.problem.get_multi_by_owner(
+        db=db, owner_id=current_user.id, skip=skip, limit=limit
+    )
+    return Problems
+
+
+@router.get('/{problem_id}', response_model=schemas.Problem)
+def read_problem_by_id(
+    Problem_id: int,
+    db: Session = Depends(deps.get_db),
+    current_user: models.User = Depends(deps.get_current_user),
+) -> Any:
+    """
+    Get a Problem by ID.
+    """
+    problem = crud.problem.get(db=db, id=Problem_id)
+    if not problem:
+        raise HTTPException(status_code=404, detail='Problem not found')
+    return problem
+
+
+@router.put('/{Problem_id}', response_model=schemas.Problem)
+def update_my_Problem(
+    *,
+    db: Session = Depends(deps.get_db),
+    problem_id: int,
+    problem_in: schemas.ProblemUpdate,
+    current_user: models.User = Depends(deps.get_current_user),
+) -> Any:
+    """
+    Update a Problem.
+    """
+    Problem = crud.Problem.get(db=db, id=problem_id)
+    if not Problem:
+        raise HTTPException(status_code=404, detail='Problem not found')
+    if not crud.user.is_superuser(current_user) and (Problem.owner_id != current_user.id):
+        raise HTTPException(status_code=400, detail='Not enough permissions')
+    Problem = crud.Problem.update(db=db, db_obj=Problem, obj_in=problem_in)
+    return Problem
+
+
+@router.delete('/{problem_id}', response_model=schemas.Problem)
+def delete_problem(
+    *,
+    db: Session = Depends(deps.get_db),
+    problem_id: int,
+    current_user: models.User = Depends(deps.get_current_user),
+) -> Any:
+    """
+    Delete a problem.
+    """
+    problem = crud.problem.get(db=db, id=problem_id)
+    if not problem:
+        raise HTTPException(status_code=404, detail='Problem not found')
+    if not crud.user.is_superuser(current_user) and (problem.owner_id != current_user.id):
+        raise HTTPException(status_code=400, detail='Not enough permissions')
+    problem = crud.problem.remove(db=db, id=problem_id)
+    return problem
+
+
+@router.get('/feed/', response_model=List[schemas.Problem])
+def read_all_problems(
+    db: Session = Depends(deps.get_db),
+    skip: int = 0,
+    limit: int = 100,
+    current_user: models.User = Depends(deps.get_current_user),
+) -> Any:
+    """
+    Get all problems.
+    """
+    problems = crud.problem.get_multi(
+        db=db, skip=skip, limit=limit
+    )
+    return problems
+
