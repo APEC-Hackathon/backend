e65a221862c9ad4b7df72b67c0090aecbc670800
nvbinh15 <binh.pdc01@gmail.com>
2023-05-27 22:36:34 +0200

    Change from dev to prod env

--- app/main.py
+++ app/main.py
@@ -8,15 +8,14 @@ from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
 
-# if settings.BACKEND_CORS_ORIGINS:
-app.add_middleware(
-    CORSMiddleware,
-    # allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_headers=["*"],
-    allow_methods=["*"],
-)
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_headers=["*"],
+        allow_methods=["*"],
+    )
 
 app.include_router(socket_router, prefix='/ws')
 app.include_router(api_router, prefix=settings.API_V1_STR)
