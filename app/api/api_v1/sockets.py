fc384f177bb9c2755d30080eaf58decf66111602
nvbinh15 <binh.pdc01@gmail.com>
2023-05-28 15:40:28 +0200

    Send translated message via socket

--- app/api/api_v1/sockets.py
+++ app/api/api_v1/sockets.py
@@ -1,7 +1,13 @@
-from app.utils.sockets import manager
+import json 
 
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
 from fastapi.websockets import WebSocketState
+from sqlalchemy.orm import Session
+
+from app.utils.sockets import manager
+from app import crud
+from app.utils.translation import translate
+from app.api import deps
 
 socket_router = APIRouter()
 
@@ -11,11 +17,16 @@ async def websocket_endpoint(
     websocket: WebSocket,
     peer_id: int,
     user_id: int,
+    db: Session = Depends(deps.get_db),
 ):
     await manager.connect(websocket, user_id, peer_id)
     try:
+        target_lang = crud.user.get(db=db, id=peer_id).prefered_language
         while websocket.application_state == WebSocketState.CONNECTED:
             data = await websocket.receive_text()
+            data = json.loads(data)
+            data['translated_content'] = translate(data['content'], target_lang)
+            data = json.dumps(data)
             await manager.send_to_peer(data, user_id, peer_id)
     except WebSocketDisconnect:
         manager.disconnect(user_id, peer_id)
